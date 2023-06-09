d50511b85fe65d4b3f28033ce915b2854fe85261
leonTech254 <martinleontech23@gmail.com>
2023-06-09 12:24:25 +0300

    --adding table data from array

--- src/main/java/org/example/CreatingTabe.java
+++ src/main/java/org/example/CreatingTabe.java
@@ -15,7 +15,7 @@ public class CreatingTabe {
         PdfDocument pdfDocument=new PdfDocument(pdfWriter);
         pdfDocument.addNewPage();
         Document document = new Document(pdfDocument);
-        float columnWidth[]={200f ,200f};
+        float[] columnWidth ={200f ,200f};
         Table table = new Table(columnWidth);
         table.addCell(new Cell().add("Movies"));
         table.addCell(new Cell().add("Rating"));
@@ -27,6 +27,41 @@ public class CreatingTabe {
         table.addCell(new Cell().add("8"));
         document.add(table);
         document.close();
+        tableUsingArray();
+
+
+    }
+
+
+    public static void tableUsingArray() throws FileNotFoundException {
+        String path="/home/leon/workspace/k/tableWithArray.pdf";
+        PdfWriter pdfWriter =new PdfWriter(path);
+        PdfDocument pdfDocument=new PdfDocument(pdfWriter);
+        pdfDocument.addNewPage();
+        Document document = new Document(pdfDocument);
+        float[] tableColumn ={100f,100f};
+        String[] tableHeader={"Movies","rattings"};
+        String[][] itemsArray = {
+                {"Blindspot", "9"},
+                {"BlacklIst", "9"},
+                {"Night Agent", "9"},
+                {"Merin", "9"}
+        };
+
+
+        Table table = new Table(tableColumn);
+        table.addCell(new Cell().add(tableHeader[0]));
+        table.addCell(new Cell().add(tableHeader[1]));
+        // Printing the values in the array
+        for (String[] item : itemsArray) {
+            table.addCell(new Cell().add(item[0]));
+            table.addCell(new Cell().add(item[1]));
+        }
+
+
+        document.add(table);
+        document.close();
+
 
 
     }
